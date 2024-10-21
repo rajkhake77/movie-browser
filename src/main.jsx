b43fffa2955077ee1c95a8eb70b8fea751181243
rajkhake77 <rajkhake89@gmail.com>
2024-10-22 00:08:59 +0530

    movieMania

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { StrictMode } from 'react'
+import ReactDOM from "react-dom/client";
+import App from './App.jsx'
+import SearchIcon from './search.svg'
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
